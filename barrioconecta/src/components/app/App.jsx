e5abb9f2ac2fab58f2101f14699daecf77149851
Analucena53 <lucenapuertasana53@gmail.com>
2024-02-20 12:59:36 +0100

    footer final

--- barrioconecta/src/components/app/App.jsx
+++ barrioconecta/src/components/app/App.jsx
@@ -1,12 +1,22 @@
+import React from 'react';
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Footer from '../footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route>
+
+        </Route>
+
+      </Routes>
+
+      <Footer />
         
     </>
   )
--- barrioconecta/src/main.jsx
+++ barrioconecta/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './components/app/App.jsx'
 import Footer from './components/footer/Footer.jsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
     <Footer />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
