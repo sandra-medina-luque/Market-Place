933d76035993c140786a1c75bfa3c7fbe4daf768
Analucena53 <lucenapuertasana53@gmail.com>
2024-02-20 12:47:07 +0100

    footer finish

--- barrioconecta/src/components/footer/Footer.jsx
+++ barrioconecta/src/components/footer/Footer.jsx
@@ -7,13 +7,14 @@ function footer() {
     return (
         <>
             <section className='footer'>
-                <p className='copy'>Barrio Conecta Copyright ©2024|Todos los derechos reservados</p>
+                <div className='textfooter'>
+                <p className='copy'>Barrio Conecta Copyright ©2024 | Todos los derechos reservados</p>
                 <p className='politica'>Aviso Legal - Política de Cookis - Política de Privacidad</p>
-
+                </div>
                 <div className='redesfooter'>
-                    <img className='instafooter' src="public/instagram.svg" alt="logo de instagram" />
-                    <img className='facefooter' src="public/facebook (1).svg" alt="logo de facebook" />
-                    <img className='twitterfooter' src="public/twitter-alt.svg" alt="logo de twitter" />
+                    <img className='instafooter' src="../public/icons nav&footer/instagram.svg" alt="logo de instagram" />
+                    <img className='facefooter' src="../public/icons nav&footer/facebook.svg" />
+                    <img className='twitterfooter' src="../public/icons nav&footer/twitter.svg" alt="logo de twitter" />
                 </div>
             </section>
         </>
