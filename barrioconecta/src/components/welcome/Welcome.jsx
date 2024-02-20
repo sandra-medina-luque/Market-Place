434df8bc459e927e5005d02aca5c791780dfe089
ilfagaro <ildafabianagaonarotela@gmail.com>
2024-02-20 13:48:12 +0100

    editar img de logo

---
+++ barrioconecta/src/components/welcome/Welcome.jsx
@@ -0,0 +1,20 @@
+import './Welcome.css'
+import React from 'react'
+import '../welcome/Welcome.css'
+import Logobarrioconecta.png from '../img-welcome/logo.png'
+
+function Welcome() {
+
+
+  return (
+    <>
+      <div >
+     
+          <img src={}></img>
+        </div>
+        
+    </>
+  )
+}
+
+export default Welcome;
--- barrioconecta/src/main.jsx
+++ barrioconecta/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import './index.css'
+import Welcome from './components/welcome/Welcome
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
