7080fd5f17f8a61df0e8864fbff98c14bfbadb0b
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-02-20 13:51:11 +0100

    validaciones

--- barrioconecta/src/components/log/Log.jsx
+++ barrioconecta/src/components/log/Log.jsx
@@ -1,34 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./log.css"
+import { UserService } from "../../../userService";
 
 
 function Log() {
-
-
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+  
+    async function getData() {
+      let user = await UserService.getUser();
+      console.log("Usuario obtenido:", user);
+    }
+  
+    useEffect(() => {
+      getData();
+    }, []);
+  
+    const handleLogin = async () => {
+      try {
+        // Llamada al servicio de autenticación
+        const response = await UserService.authenticateUser(username, password);
+  
+        // Lógica para manejar la respuesta del servicio
+        if (response.status === 200) {
+          // Autenticación exitosa, puedes redirigir o manejar el resultado de otra manera
+          console.log("Inicio de sesión exitoso");
+        } else {
+          // Autenticación fallida
+          console.error("Error al iniciar sesión");
+        }
+      } catch (error) {
+        // Manejar errores de la llamada al servicio
+        console.error("Error al llamar al servicio de autenticación", error);
+      }
+    };
+  
     return (
-
-        <>
-            <body className="logcont">
-                <section className="logform">
-                    <div class="mb-3">
-                        <label for="formGroupInput" className="form-label">Usuario:</label>
-                        <input type="text" className="form-control" />
-                    </div>
-                    <div class="mb-4">
-                        <label for="formGroupInput" className="form-label">Contraseña:</label>
-                        <input type="password" class="form-control"/>
-                    </div>
-
-                    </section>
-                    <button> Conectar</button>
-            </body>
-
-        </>
-
-
-    )
-}
-
-export default Log;
-
-
+      <>
+        <body className="logcont">
+          <section className="logform">
+            <div className="mb-3">
+              <label htmlFor="formGroupInput" className="form-label">
+                Usuario:
+              </label>
+              <input
+                type="text"
+                name="userName"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="formGroupInput" className="form-label">
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </section>
+          <button onClick={handleLogin}>Conectar</button>
+        </body>
+      </>
+    );
+  }
+  
+  export default Log;
---
+++ barrioconecta/userService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3000/',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+export const UserService = {
+
+    async getUser() {
+
+        let response = await apiClient.get("/user");
+        let User = response.data
+
+        return User
+    },
+
+    async authenticateUser(username, password) {
+        try {
+            const response = await apiClient.post("/login", {
+                username,
+                password,
+            });
+
+            // Puedes devolver la respuesta completa o solo los datos necesarios
+            return response;
+        } catch (error) {
+            // Manejar errores aquí
+            throw error;
+        }
+    },
+};
+
+
+
