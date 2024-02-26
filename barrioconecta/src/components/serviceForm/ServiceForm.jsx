18e19d8fef2b893f455ee68d0e2df939458ad5ed
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-02-26 14:45:53 +0100

    serviceForm

---
+++ barrioconecta/src/components/serviceForm/ServiceForm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import '../serviceForm/serviceForm.css'
+import { userService } from "../../../userService";
+import Swal from 'sweetalert2';
+
+
+
+function ServiceForm() {
+    const [service, setService] = useState({
+        name: '',
+        description: '',
+        image: null,
+        price: '',
+    });
+
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            const uploadPromise = cloudinary.uploader.upload(file);
+
+            uploadPromise.then((result) => {
+                setService({ ...service, image: result.secure_url });
+            });
+        },
+    });
+
+    useEffect(() => { getData(); }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const createdService = await userService(service);
+        console.log('Servicio creado:', createdService);
+
+        // Reiniciar el formulario
+        setService({ name: '', description: '', image: null, price: '' });
+
+        Swal.fire({
+            icon: 'success',
+            title: '¡Éxito!',
+            text: 'Se ha creado correctamente el servicio de usuario.',
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Arrastre y suelte algunos archivos aquí, o haga clic para seleccionar archivos</p>
+            </div>
+            <label htmlFor="name">Nombre:</label>
+            <input
+                type="text"
+                id="name"
+                value={service.name}
+                onChange={(e) => setService({ ...service, name: e.target.value })}
+            />
+            <label htmlFor="description">Descripción:</label>
+            <textarea
+                id="description"
+                value={service.description}
+                onChange={(e) =>
+                    setService({ ...service, description: e.target.value })
+                }
+            />
+            <label htmlFor="price">Precio:</label>
+            <input
+                type="text"
+                id="price"
+                value={service.price}
+                onChange={(e) => setService({ ...service, price: e.target.value })}
+            />
+            <button type="submit">Crear servicio</button>
+        </form>
+    );
+};
+export default ServiceForm;
---
+++ barrioconecta/userService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3000/',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+
+export const UserService = {
+
+    async getUser() {
+
+        let response = await apiClient.get("/user");
+        let User = response.data
+
+        return User
+    },
+
+
+};
+
+export const createService = async (service) => {
+    const response = await axios.post(`${API_URL}/service`, service);
+    return response.data;
+};
