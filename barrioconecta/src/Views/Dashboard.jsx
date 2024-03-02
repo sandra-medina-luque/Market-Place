51418e2753ca7e5f1672bb69825392d099d98b9a
Analucena53 <lucenapuertasana53@gmail.com>
2024-03-03 00:14:09 +0100

    navbar2 y footer en dashboard

--- barrioconecta/src/Views/Dashboard.jsx
+++ barrioconecta/src/Views/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar2 from '../components/navbar2/Navbar2'
-
+import Footer from '../components/footer/Footer';
 
 
 const Dashboard = () => {
@@ -8,6 +8,7 @@ const Dashboard = () => {
         <>
             <Navbar2 />
             <h1>Acá va el Dashboard</h1>
+            <Footer />
         </>
     );
 }
