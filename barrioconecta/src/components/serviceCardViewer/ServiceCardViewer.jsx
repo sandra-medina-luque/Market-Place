1f4e57348e7d30d8d4e0a9d6e6b0a33841fc7703
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-03-05 13:18:00 +0100

    single page

--- barrioconecta/src/components/serviceCardViewer/ServiceCardViewer.jsx
+++ barrioconecta/src/components/serviceCardViewer/ServiceCardViewer.jsx
@@ -9,10 +9,13 @@ import intermedio from '../../assets/img/intermedio.png'
 import avanzado from '../../assets/img/avanzado.png'
 import certificado from '../../assets/img/certificado.png'
 import '../serviceCardViewer/serviceCardViewer.css'
+import reserva from '../../assets/img/Reserva.png'
+
 
 function ServiceCardViewer({ userServices }) {
     const [updatedUserServices, setUpdatedUserServices] = useState([]);
     const [filterCategory, setFilterCategory] = useState('all');
+    const [selectedService, setSelectedService] = useState(null);
 
     useEffect(() => {
         setUpdatedUserServices([...userServices]);
@@ -36,6 +39,14 @@ function ServiceCardViewer({ userServices }) {
         setFilterCategory(category);
     };
 
+    const handleCardClick = (service) => {
+        setSelectedService(service);
+    };
+
+    const closeDetailView = () => {
+        setSelectedService(null);
+    };
+
     const filteredUserServices = filterCategory === 'all'
         ? updatedUserServices
         : updatedUserServices.filter(service => service.category.toLowerCase() === filterCategory.toLowerCase());
@@ -46,32 +57,32 @@ function ServiceCardViewer({ userServices }) {
                 <div>
                     <h2>Todos</h2>
                     <button className="buttoncategory" onClick={() => handleFilterCategory('all')}>
-                    <img src={all} alt="alllogo" />
+                        <img src={all} alt="alllogo" />
                     </button>
                 </div>
                 <div>
                     <h2>Básico</h2>
-                <button className="buttoncategory" onClick={() => handleFilterCategory('básico')}>
-                    <img src={basico} alt="basicologo" />
-                </button>
+                    <button className="buttoncategory" onClick={() => handleFilterCategory('básico')}>
+                        <img src={basico} alt="basicologo" />
+                    </button>
                 </div>
                 <div>
-                <h2>Medio</h2>
-                <button className="buttoncategory" onClick={() => handleFilterCategory('medio')}>
-                    <img src={intermedio} alt="intermediologo" />
-                </button>
+                    <h2>Medio</h2>
+                    <button className="buttoncategory" onClick={() => handleFilterCategory('medio')}>
+                        <img src={intermedio} alt="intermediologo" />
+                    </button>
                 </div>
                 <div>
-                <h2>Avanzado</h2>
-                <button className="buttoncategory" onClick={() => handleFilterCategory('avanzado')}>
-                    <img src={avanzado} alt="avanzadologo" />
-                </button>
+                    <h2>Avanzado</h2>
+                    <button className="buttoncategory" onClick={() => handleFilterCategory('avanzado')}>
+                        <img src={avanzado} alt="avanzadologo" />
+                    </button>
                 </div>
                 <div>
-                <h2>Certificados</h2>
-                <button className="buttoncategory" onClick={() => handleFilterCategory('certificado')}>
-                    <img src={certificado} alt="certificadologo" />
-                </button>
+                    <h2>Certificados</h2>
+                    <button className="buttoncategory" onClick={() => handleFilterCategory('certificado')}>
+                        <img src={certificado} alt="certificadologo" />
+                    </button>
                 </div>
             </div>
 
@@ -83,10 +94,27 @@ function ServiceCardViewer({ userServices }) {
                         imageUrl={createdService.image}
                         modoEdicion={false}
                         onDecrementStock={() => handleDecrementStock(createdService.id)}
+                        onCardClick={() => handleCardClick(createdService)}
                     />
                 ))}
             </div>
+
+            {selectedService && (
+                <div className="detailed-view">
+                    <h2>{selectedService.name}</h2>
+                    <img src={selectedService.image} alt={selectedService.name} style={{ width: '100%', height: '500px', objectFit: 'cover' }} />
+                    <p>{selectedService.description}</p>
+                    <p>Precio: {selectedService.price} €/hora</p>
+                    <p>Categoría: {selectedService.category}</p>
+                    <p>Stock: {selectedService.stock} horas</p>
+                    <button onClick={() => handleDecrementStock(selectedService.id)}>
+                        <img src={reserva} alt="reserlog" />
+                    </button>
+                    <button onClick={closeDetailView}>Cerrar Detalles</button>
+                </div>
+            )}
         </div>
+
     );
 }
 
--- barrioconecta/src/components/userServiceCard/UserServiceCard.jsx
+++ barrioconecta/src/components/userServiceCard/UserServiceCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { userService } from '../../../userService';
+import { useState } from 'react';
 import '../userServiceCard/userServiceCard.css'
 import basura from '../../assets/img/basura.png'
 import editar from '../../assets/img/editar.png'
 import reserva from '../../assets/img/Reserva.png'
 
-const UserServiceCard = ({ userService, imageUrl, onDelete, onEdit, modoEdicion, onDecrementStock }) => {
+const UserServiceCard = ({ userService, imageUrl, onDelete, onEdit, modoEdicion, onDecrementStock, onCardClick}) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleCardClick = () => {
+    setShowDetails(true);
+    if (typeof onCardClick === 'function') {
+      onCardClick(userService);
+    }
+  };
+
+  
+
   return (
-    <div className="col-md-3 mb-3">
+    <div className="col-md-3 mb-3" onClick={handleCardClick}>
       <div className="card h-100">
         <img src={imageUrl} className="card-img-top" alt={userService.name} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
         <div className="card-body">
@@ -19,24 +30,24 @@ const UserServiceCard = ({ userService, imageUrl, onDelete, onEdit, modoEdicion,
         </div>
         {modoEdicion && (
           <div className='buttons'>
-            <button className='buttons' onClick={() => onDelete(userService.id)}>
+            <button className='buttons' onClick={(e) => { e.stopPropagation(); onDelete(userService.id); }}>
               <img src={basura} alt="basuralog" />
             </button>
-            <button className='buttons' onClick={() => onEdit(userService.id)}>
+            <button className='buttons' onClick={(e) => { e.stopPropagation(); onEdit(userService.id); }}>
               <img src={editar} alt="editlogo" />
             </button>
           </div>
         )}
         {!modoEdicion && (
           <div className="buttons">
-            <button className='buttons' onClick={typeof onDecrementStock === 'function' ? onDecrementStock : () => { }}>
+            <button className='buttons' onClick={(e) => { e.stopPropagation(); typeof onDecrementStock === 'function' && onDecrementStock(); }}>
               <img src={reserva} alt="reserlog" />
             </button>
-
           </div>
         )}
       </div>
 
+      
     </div>
   );
 };
